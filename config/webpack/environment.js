91db6c707a50670da62a279da306229a4aade320
SHIMADA Keiki <shimada.cake@gmail.com>
2019-10-28 17:35:40 +0900

    ログイン画面とログイン機能を実装

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -2,4 +2,12 @@ const { environment } = require('@rails/webpacker')
 const elm =  require('./loaders/elm')
 
 environment.loaders.prepend('elm', elm)
+
+const webpack = require('webpack');
+environment.plugins.append('Provide', new webpack.ProvidePlugin({
+    $: 'jquery',
+    jQuery: 'jquery',
+    Popper: ['popper.js', 'default']
+}));
+
 module.exports = environment
