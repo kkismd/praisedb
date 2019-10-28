5d793cfaa819b9b9a78e264a6d31261397a4d52b
SHIMADA Keiki <shimada.cake@gmail.com>
2019-10-28 19:06:27 +0900

    非ログイン時の挙動

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,11 @@
 class ApplicationController < ActionController::Base
   include SessionsHelper
+  before_action :logged_in_user
+
+  def logged_in_user
+    unless logged_in?
+      flash[:danger] = 'ログインして下さい'
+      redirect_to login_path
+    end
+  end
 end
--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,10 +1,12 @@
 module SessionsHelper
   def log_in(user)
+    reset_session
     session[:user_id] = user.id
   end
 
   def log_out
     session.delete(:user_id)
+    reset_session
     @current_user = nil
   end
 
