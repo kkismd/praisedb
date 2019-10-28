ce1edcd1ce88c1a32e5809aabfd34b9c7f221700
SHIMADA Keiki <shimada.cake@gmail.com>
2019-10-28 19:06:27 +0900

    非ログイン時の挙動

--- app/controllers/top_controller.rb
+++ app/controllers/top_controller.rb
@@ -1,4 +1,6 @@
 class TopController < ApplicationController
+  before_action :logged_in_user, only: []
+
   def index
   end
 end
--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -6,16 +6,19 @@ class UsersControllerTest < ActionDispatch::IntegrationTest
   end
 
   test "should get index" do
+    log_in_as(@user)
     get users_url
     assert_response :success
   end
 
   test "should get new" do
+    log_in_as(@user)
     get new_user_url
     assert_response :success
   end
 
   test "should create user" do
+    log_in_as(@user)
     assert_difference('User.count') do
       post users_url, params: { user: { name: @user.name, password_digest: @user.password_digest, status: @user.status } }
     end
@@ -24,25 +27,41 @@ class UsersControllerTest < ActionDispatch::IntegrationTest
   end
 
   test "should show user" do
+    log_in_as(@user)
     get user_url(@user)
     assert_response :success
   end
 
   test "should get edit" do
+    log_in_as(@user)
     get edit_user_url(@user)
     assert_response :success
   end
 
   test "should update user" do
+    log_in_as(@user)
     patch user_url(@user), params: { user: { name: @user.name, password_digest: @user.password_digest, status: @user.status } }
     assert_redirected_to user_url(@user)
   end
 
   test "should destroy user" do
+    log_in_as(@user)
     assert_difference('User.count', -1) do
       delete user_url(@user)
     end
 
     assert_redirected_to users_url
+  end
+
+  test "should redirect edit when not logged in" do
+    get edit_user_path(@user)
+    assert_not flash.empty?
+    assert_redirected_to login_url
+  end
+
+  test "should redirect update when not logged in" do
+    patch user_path(@user), params: { user: { name: @user.name } }
+    assert_not flash.empty?
+    assert_redirected_to login_url
   end
 end
