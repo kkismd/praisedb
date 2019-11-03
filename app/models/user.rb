c2f6aa3871da3de3dc3c3ee986fa3d79e285db33
SHIMADA Keiki <shimada.cake@gmail.com>
2019-11-03 12:39:44 +0900

    ログイン、ログアウトのリンク、ユーザー権限

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,7 @@
 class User < ApplicationRecord
+  validates :name, presence: true
   has_secure_password
+  enum status: { normal: 0, disabled: 9, admin: 10, admin_disabled: 19 }
 
   def self.digest(str)
     cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST : BCrypt::Engine.cost
