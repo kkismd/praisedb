fdb6f8d2e1524792cd7e9644cf2a97ef7cd4e56f
SHIMADA Keiki <shimada.cake@gmail.com>
2019-11-06 08:49:52 +0900

    songs修正

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
-  root to: "top#index"
+  root to: 'top#index'
 
-  get "/login", to: "sessions#new"
-  post "/login", to: "sessions#create"
-  delete "logout", to: "sessions#destroy"
+  get '/login', to: 'sessions#new'
+  post '/login', to: 'sessions#create'
+  delete 'logout', to: 'sessions#destroy'
 
   resources :organizations
   resources :users
@@ -17,6 +17,35 @@ Rails.application.routes.draw do
       get :search
       get :list
       get :list_all
+      post :preview
+    end
+  end
+
+  resources :folders do
+    member do
+      get :content, :as => :content_of
+      post :reorder, :as => :reorder
+    end
+    collection do
+      post :set_current, :as => :set_current
+      post :create_remote, :as => :add
+    end
+  end
+
+  resources :books do
+    collection do
+      get :search
+    end
+  end
+
+  resources :slides do
+    member do
+      get :detail
+    end
+
+    collection do
+      get :search
+      get :list
       post :preview
     end
   end
