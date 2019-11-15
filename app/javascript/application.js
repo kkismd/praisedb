9055b3853581ef76ced5ac998be0935322786194
SHIMADA Keiki <shimada.cake@gmail.com>
2019-11-15 19:11:27 +0900

    add app container

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -2,27 +2,30 @@ import 'bootstrap';
 
 import "./application.scss";
 import $ from "jquery";
+window.$ = $;
 import "jquery-ui/accordion";
 import "jquery-ui/sortable";
 import "jquery-ui/datepicker";
 import "jquery-ui/draggable";
 import "jquery-ui-themes/themes/dark-hive/jquery-ui.min.css";
 import "jquery-ui-themes/themes/dark-hive/theme.css";
-
-window.$ = $;
-
 import Rails from "rails-ujs";
 Rails.start();
-
-$.ajaxSetup({
-    headers: {
-        'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
-    }
-});
 
 import './common';
 import './books';
 import './songs';
+import ready from './ready';
+
+// start up your app
+ready(function () {
+    $.ajaxSetup({
+        headers: {
+            'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
+        }
+    });
+});
+
 
 /** ここから下はサンプル */
 import { hello } from "./greeter";
