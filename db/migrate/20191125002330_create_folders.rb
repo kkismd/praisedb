0da8fb0fac52254255e5892c57ef673ef864f52e
SHIMADA Keiki <shimada.cake@gmail.com>
2019-11-25 00:28:58 +0900

    各テーブルを組織に紐付け

--- db/migrate/20191125002330_create_folders.rb
+++ db/migrate/20191125002330_create_folders.rb
@@ -1,6 +1,7 @@
 class CreateFolders < ActiveRecord::Migration[6.0]
   def change
     create_table :folders do |t|
+      t.references :organization, foreign_key: true
       t.string :title, null: false
       t.boolean :sticky, null: false, default: false
 
